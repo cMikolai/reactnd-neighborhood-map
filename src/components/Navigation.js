4d28e3e723176a7c9fed0d8ab28cadca5ed3e1e9
Christin Mikolai <mail@christinmikolai.de>
2018-07-22 19:25:34 +0200

    feat: Navigation file change

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
+import '../App.css';
 
 class Navigation extends Component {
   render() {
     return (
       <div>
-
-        
-
+        <header className="App-header"></header>
       </div>
     )
   }
