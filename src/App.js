1ca33baf435eba4b9e1c0ab64cdbacee0e547ce6
Christin Mikolai <mail@christinmikolai.de>
2018-07-22 18:39:21 +0200

    feat: Add navigation-bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Map from './components/Map';
+import Navigation from './components/Navigation';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,12 +8,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="Sidebar">
+
+        <header className="App-header">
+          <Navigation />
+        </header>
+
+        <div className="Sidebar">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">My Neighborhood</h1>
           <input
           className='' />
-        </header>
+        </div>
 
         <div className="Map-container">
           <Map />
