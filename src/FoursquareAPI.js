4e2698ac1a61d4e6a993fbe24ac13b16e5d32fcc
Christin Mikolai <mail@christinmikolai.de>
2018-07-30 22:34:40 +0200

    adding basic foursquare file

---
+++ src/FoursquareAPI.js
@@ -0,0 +1,22 @@
+//Using the client ID and secret obtained previously, lets make our first API call!. The examples below show you how to find a coffee shop near a given location.
+
+const request = require('request');
+
+request({
+  url: 'https://api.foursquare.com/v2/venues/explore',
+  method: 'GET',
+  qs: {
+    client_id: 'AU4JNRCBGSTSHHAKB0KU3WIA5ZNTPV2DYD1QUEE5DZMRCXTF',
+    client_secret: 'VA0YLV21BIMVDZCSWATVUSX2D2Q2RSVUFYS5VCZQO0ZXEBXE',
+    ll: '40.7243,-74.0018',
+    query: 'coffee',
+    v: '20180323',
+    limit: 1
+  }
+}, function(err, res, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    //console.log(body);
+  }
+});
