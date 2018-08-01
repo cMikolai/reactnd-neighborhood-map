fa71b9b8161e02c2aeed0c4f47e629f5b768e3ef
Christin Mikolai <mail@christinmikolai.de>
2018-08-01 02:56:32 +0200

    example API fetch

--- src/FoursquareAPI.js
+++ src/FoursquareAPI.js
@@ -3,20 +3,20 @@
 const request = require('request');
 
 request({
-  url: 'https://api.foursquare.com/v2/venues/explore',
-  method: 'GET',
-  qs: {
-    client_id: 'AU4JNRCBGSTSHHAKB0KU3WIA5ZNTPV2DYD1QUEE5DZMRCXTF',
-    client_secret: 'VA0YLV21BIMVDZCSWATVUSX2D2Q2RSVUFYS5VCZQO0ZXEBXE',
-    ll: '40.7243,-74.0018',
-    query: '',
-    v: '20180323',
-    limit: 1
-  }
+ url: 'https://api.foursquare.com/v2/venues/explore',
+ method: 'GET',
+ qs: {
+   client_id: 'AU4JNRCBGSTSHHAKB0KU3WIA5ZNTPV2DYD1QUEE5DZMRCXTF',
+   client_secret: 'VA0YLV21BIMVDZCSWATVUSX2D2Q2RSVUFYS5VCZQO0ZXEBXE',
+   ll: '40.7243,-74.0018',
+   query: '',
+   v: '20180323',
+   limit: 1
+ }
 }, function(err, res, body) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(body);
-  }
+ if (err) {
+   console.error(err);
+ } else {
+   //console.log(body);
+ }
 });
